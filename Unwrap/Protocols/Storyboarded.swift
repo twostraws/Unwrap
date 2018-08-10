6f3194eb03295215febed3fd6c9a3abe86450489
Rizwan Mohamed Ibrahim <rizwankce@users.noreply.github.com>
2018-08-10 12:30:36 +0800

    [ADD] Use Swift String describing method to get viewcontroller name

--- Unwrap/Protocols/Storyboarded.swift
+++ Unwrap/Protocols/Storyboarded.swift
@@ -8,17 +8,16 @@
 
 import UIKit
 
-/// A protocol that lets us create view controllers from our storyboard.
+/// A protocol that lets us instantiate view controllers from Main storyboard.
 protocol Storyboarded { }
 
 extension Storyboarded where Self: UIViewController {
     // Creates a view controller from our storyboard. This relies on view controllers having the same storyboard identifier as their class name. This method shouldn't be overridden in conforming types.
     static func instantiate() -> Self {
-        let fullName = NSStringFromClass(self)
-        let className = fullName.components(separatedBy: ".")[1]
+        let storyboardIdentifier = String(describing: self)
         let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
 
         // swiftlint:disable:next force_cast
-        return storyboard.instantiateViewController(withIdentifier: className) as! Self
+        return storyboard.instantiateViewController(withIdentifier: storyboardIdentifier) as! Self
     }
 }
