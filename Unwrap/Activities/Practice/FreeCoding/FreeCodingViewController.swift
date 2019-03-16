2a40d3a18fc94cdf087ffe894d43d717a963357d
Paul Hudson <paul.hudson@gmail.com>
2019-03-16 21:24:59 +0000

    Made Free Coding try again alert use SwiftEntryKit.

--- Unwrap/Activities/Practice/FreeCoding/FreeCodingViewController.swift
+++ Unwrap/Activities/Practice/FreeCoding/FreeCodingViewController.swift
@@ -98,14 +98,7 @@ class FreeCodingViewController: UIViewController, Storyboarded, PracticingViewCo
 
     /// Give users the choice of trying again or skipping
     func skipOrRetry() {
-        let ac = UIAlertController(title: "That's not quite right!", message: "Check your code carefully, and try going for the simplest solution that works.", preferredStyle: .alert)
-
-        ac.addAction(UIAlertAction(title: "OK", style: .default) { [weak self] action in
-            self?.answerButton.backgroundColor = UIColor(bundleName: "Primary")
-            self?.answerButton.setImage(nil, for: .normal)
-        })
-
-        present(ac, animated: true)
+        showAlert(title: "That's not quite right!", body: "Check your code carefully, and try going for the simplest solution that works.", coordinator: nil, alternateTitle: nil, alternateAction: nil)
     }
 
     /// Allows users to dismiss the keyboard when they are ready, so they can tap submit
