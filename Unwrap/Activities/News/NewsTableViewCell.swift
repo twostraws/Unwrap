b81e09ca784fe6cd561ea0da85a202eabcc55a2f
Paul Hudson <paul.hudson@gmail.com>
2019-04-08 00:51:57 +0100

    Updated design for unread buttons in News tab.

--- Unwrap/Activities/News/NewsTableViewCell.swift
+++ Unwrap/Activities/News/NewsTableViewCell.swift
@@ -10,7 +10,26 @@ import UIKit
 
 /// Represents a row in the News table view that contains an article to read.
 class NewsTableViewCell: UITableViewCell {
+    // custom connections for the image view, title, and subtitle
+    @IBOutlet var customImageView: UIImageView!
+    @IBOutlet var customTextLabel: UILabel!
+    @IBOutlet var customDetailTextLabel: UILabel!
 
-    /// The little splat beside the image indicating if the article has been read or not.
+    /// The dot beside the image indicating if the article has been read or not.
     @IBOutlet weak var readNotification: UILabel!
+
+    // send back our custom image view in place of the default image view
+    override var imageView: UIImageView? {
+        return customImageView
+    }
+
+    // send back our custom text label in place of the default text label
+    override var textLabel: UILabel? {
+        return customTextLabel
+    }
+
+    // send back our custom detail text label in place of the default detail text label
+    override var detailTextLabel: UILabel? {
+        return customDetailTextLabel
+    }
 }
