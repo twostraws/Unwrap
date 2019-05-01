d92d0bc28b8b692a95054dc22cd2b36936a48a48
Paul Hudson <paul.hudson@gmail.com>
2019-05-01 16:39:05 +0100

    Enabled Auto Layout constraints in a batch.

--- Unwrap/Activities/Home/Help/HelpTableViewCell.swift
+++ Unwrap/Activities/Home/Help/HelpTableViewCell.swift
@@ -14,20 +14,21 @@ class HelpTableViewCell: UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
 
+        textView.translatesAutoresizingMaskIntoConstraints = false
         textView.font = Unwrap.scaledBaseFont
-
         textView.isEditable = false
         textView.isScrollEnabled = false
         textView.dataDetectorTypes = [.link]
-
         contentView.addSubview(textView)
-        textView.translatesAutoresizingMaskIntoConstraints = false
-        textView.topAnchor.constraint(equalTo: contentView.layoutMarginsGuide.topAnchor).isActive = true
-        textView.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor).isActive = true
-        textView.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor).isActive = true
 
-        // Add extra spacing at the bottom of the cell to provide clearer section spacing
-        textView.bottomAnchor.constraint(equalTo: contentView.layoutMarginsGuide.bottomAnchor, constant: -20).isActive = true
+        NSLayoutConstraint.activate([
+            textView.topAnchor.constraint(equalTo: contentView.layoutMarginsGuide.topAnchor),
+            textView.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor),
+            textView.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor),
+
+            // Add extra spacing at the bottom of the cell to provide clearer section spacing
+            textView.bottomAnchor.constraint(equalTo: contentView.layoutMarginsGuide.bottomAnchor, constant: -20)
+        ])
     }
 
     required init?(coder aDecoder: NSCoder) {
