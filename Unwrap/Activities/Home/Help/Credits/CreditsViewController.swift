a29f8337dca4a6fbb888c32c2bf418945d7ecb23
Wayne Nihart <wayne@waynenihart.io>
2019-05-07 09:21:27 -0600

    Modified VC

--- Unwrap/Activities/Home/Help/Credits/CreditsViewController.swift
+++ Unwrap/Activities/Home/Help/Credits/CreditsViewController.swift
@@ -10,13 +10,13 @@ import UIKit
 import WebKit
 
 class CreditsViewController: UIViewController {
+    
     private lazy var webview: WKWebView = {
     let preferences = WKPreferences()
     preferences.javaScriptEnabled = false
     let configuration = WKWebViewConfiguration()
     configuration.preferences = preferences
     let webview = WKWebView(frame: .zero, configuration: configuration)
-    webview.translatesAutoresizingMaskIntoConstraints = false
     return webview
   }()
 
@@ -26,34 +26,14 @@ class CreditsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        setupViews()
-    loadHTML("Credits.html")
+        
+        if let url = Bundle.main.url(forResource: "help", withExtension: "html") { webView.loadFileURL(url, allowingReadAccessTo: url.deletingLastPathComponent()) 
+        }
     
-    NotificationCenter.default.addObserver(self, 
-  selector: #selector(contentSizeDidChange(_:)),
-  name: NSNotification.Name.UIContentSizeCategoryDidChange,
-  object: nil)
+    NotificationCenter.default.addObserver(self, selector: #selector(contentSizeDidChange(_:)), name: NSNotification.Name.UIContentSizeCategoryDidChange, object: nil)
     }
-    
-    private func setupViews() {
-    view.addSubview(webview)
-    NSLayoutConstraint.activate([
-        webview.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-        webview.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-        webview.topAnchor.constraint(equalTo: view.topAnchor),
-        webview.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-        ])
-  }
-  
-  private func loadHTML(_ file: String) {
-    if let baseURL = Bundle.main.resourceURL {
-      let fileURL = baseURL.appendingPathComponent(file)
-      webview.loadFileURL(fileURL, allowingReadAccessTo: fileURL)
-    }
-  }
   
   @objc private func contentSizeDidChange(_ notification: Notification) {
-    webview.reload()
-}
+    webview.reload() 
+    }
 }
