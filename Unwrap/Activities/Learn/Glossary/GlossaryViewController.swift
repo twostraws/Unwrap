7fddf285074b4c3945c500629e7ff07c5132829d
Marton Zeisler <marton.zeisler@gmail.com>
2020-02-27 18:09:42 +0000

    Added Search Functionality to Glossary Screen
    Added a UISearchController to the navigation bar, the search bar is always visible similar to iOS built-in apps. If no results are found after a search, a simple "No Results" label is displayed on the screen.

--- Unwrap/Activities/Learn/Glossary/GlossaryViewController.swift
+++ Unwrap/Activities/Learn/Glossary/GlossaryViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class GlossaryViewController: UITableViewController {
     let dataSource = GlossaryDataSource()
 
+    let noResultsLabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,7 +20,38 @@ class GlossaryViewController: UITableViewController {
         extendedLayoutIncludesOpaqueBars = true
         navigationItem.largeTitleDisplayMode = .never
 
+        dataSource.delegate = self
         tableView.dataSource = dataSource
         tableView.register(GlossaryTableViewCell.self, forCellReuseIdentifier: "Cell")
+        tableView.tableFooterView = UIView() // Remove separator lines when search has no results
+
+        setupNoResultsLabel()
+        setupSearchController()
     }
+    
+    func setupNoResultsLabel() {
+        noResultsLabel.text = "No Results"
+        noResultsLabel.font = Unwrap.scaledBoldFont
+        noResultsLabel.isHidden = true
+        
+        view.addSubview(noResultsLabel)
+        noResultsLabel.translatesAutoresizingMaskIntoConstraints = false
+        noResultsLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30).isActive = true
+        noResultsLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    
+    func setupSearchController() {
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = dataSource
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search"
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+    }
+    
+    func searchPerformed(noResults: Bool) {
+        tableView.reloadData()
+        noResultsLabel.isHidden = !noResults
+    }
+
 }
